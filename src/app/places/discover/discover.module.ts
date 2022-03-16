48e69aee6c5f78d5cd58d08393bf317c09d3e8c8
sone <sthaya@gmail.com>
2022-03-16 10:57:12 -0700

    setup routing for discover and offer pages

--- src/app/places/discover/discover.module.ts
+++ src/app/places/discover/discover.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 
 import { IonicModule } from '@ionic/angular';
 
 import { DiscoverPageRoutingModule } from './discover-routing.module';
 
 import { DiscoverPage } from './discover.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DiscoverPage
+  }
+];
 
 @NgModule({
   imports: [
