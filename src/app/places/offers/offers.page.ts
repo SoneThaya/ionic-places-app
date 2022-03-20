56caf8400be09a4d009a7903fb98e9c39fcfa485
sone <sthaya@gmail.com>
2022-03-19 19:51:20 -0700

    setup slider for edit offer page

--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { IonItemSliding } from '@ionic/angular';
 import { Place } from '../place.model';
 import { PlacesService } from '../places.service';
 
@@ -10,10 +12,15 @@ import { PlacesService } from '../places.service';
 export class OffersPage implements OnInit {
   offers: Place[];
 
-  constructor(private placesServive: PlacesService) { }
+  constructor(private placesServive: PlacesService, private router: Router) {}
 
   ngOnInit() {
     this.offers = this.placesServive.places;
   }
 
+  onEdit(offerId: string, slidingItem: IonItemSliding) {
+    slidingItem.close();
+    this.router.navigate(['/', 'places', 'tabs', 'offers', 'edit', offerId]);
+    console.log('editing item', offerId);
+  }
 }
